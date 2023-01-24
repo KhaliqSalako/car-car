a728d9098c58ac80b63bfbd009a992f0b36a1074
Khaliq <itskhaliq@icloud.com>
2023-01-24 09:10:46 -0600

    updated urls file

--- service/api/service_rest/urls.py
+++ service/api/service_rest/urls.py
@@ -8,4 +8,6 @@ from .views import api_appointment, api_servicetechnician, api_appointments, api
 urlpatterns = [
     path("service_technician/", api_servicetechnicians, name="api_servicetechnicians"),
     path("appointments/", api_appointments, name="api_appointments"),
+    path("service_technician/<int:pk>/", api_servicetechnician, name="service_technician"),
+    path("appointments/<int:pk>/", api_appointment, name="appointment"),
 ]
