4a22f7a401e9c2d4e9065bd3db25ad8a37dbf86f
Khaliq <itskhaliq@icloud.com>
2023-01-25 19:00:36 -0600

    updated form

--- ghi/app/src/ServiceComponents/ServiceAppointmentList.js
+++ ghi/app/src/ServiceComponents/ServiceAppointmentList.js
@@ -32,6 +32,7 @@ function ServiceAppointmentList() {
         const url = 'http://localhost:8080/api/appointments/'
         const fetchConfig = {
             method: "PUT",
+            body: JSON.stringify(data),
             headers: {
                 "Content-Type": "application/json"
             }
