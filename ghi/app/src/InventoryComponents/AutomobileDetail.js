e7bc3a480fa246e9cfeeddc6fe6a09713d99c2e4
Kenneth <kzheng177@gmail.com>
2023-02-21 08:00:15 -0500

    automobile details

--- ghi/app/src/InventoryComponents/AutomobileDetail.js
+++ ghi/app/src/InventoryComponents/AutomobileDetail.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 function AutomobileDetails() {
-  const [automobile, setAutomobile] = useState([]);
+  const [automobile, setAutomobile] = useState({});
   const {vin} = useParams();
 
   const getData = async () => {
@@ -23,6 +23,9 @@ function AutomobileDetails() {
       <p>VIN: {automobile.vin}</p>
       <p>Year: {automobile.year}</p>
       <p>Color: {automobile.color}</p>
+      <p>Manufacturer: {automobile.model?.manufacturer.name}</p>
+      <p>Model: {automobile.model?.name} </p>
+      <img src={automobile.model?.picture_url} width="1000" />
     </div>
   );
 }
